62591f71e50b6a5318d53872fd4efb196f349bcd
nienie0226-dot <nienie0226@gmail.com>
2026-05-12 11:04:21 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -7,6 +7,7 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+from flask import Flask, render_template, request, make_response, jsonify
 
 
 def get_firestore_client():
@@ -486,6 +487,18 @@ def weather():
 
     R += "<a href='/weather'>重新查詢</a> | <a href='/'>回到首頁</a>"
     return R
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    
+    return make_response(jsonify({"fulfillmentText": info}))
+
  
 if __name__ == "__main__":
     app.run()
